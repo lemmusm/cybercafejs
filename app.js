a05cf3c1d0bb881174341110e8a733ff012fb35a
lemmusm <lemmusm@gmail.com>
2019-11-13 16:12:24 -0600

    v0.0.3
    	.- Fix error on mobile devices

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-// VARIABLES
 
+// Declarar variables
 const formularioUI = document.querySelector('#formulario');
-const listaUsuariosUI= document.querySelector('#listaUsuarios');
+const listaUsuariosUI = document.querySelector('#listaUsuarios');
 const limpiarUI = document.querySelector('#limpiarCache');
 
+// Arreglo para guardar
 let arrayUsuarios = [];
 
 // FUNCIONES
-CrearUsuario = (usuario) => {
 
-    // let time = $inicio.toTimeString().substring(0, time.indexOf(' '));
+/*  Creando usuario */
+CrearUsuario = usuario => {
+
+    // Objeto para manejar datos del usuario
     usuario = {
         usuario: usuario,
         inicio: moment().format('hh:mm:ss a'),
-        inicioTiempo: moment().format('DD/MM/YYYY HH:mm:ss'),
+        inicioTiempo: new Date(),
         fin: '0',
         finTiempo: '0',
         tiempo: '0',
         tarifa: '0'
-    }
+    };
+    // Agregamos objeto al array
     arrayUsuarios.push(usuario);
-
+    //Regresamos al usuario
     return usuario;
 }
 
+/*  Guarda usuario en local storage, al mismo tiempo con el método MostrarData() trae la la vista los datos guardados */
 GuardarUsuario = () => {
-    localStorage.setItem('usuarios', JSON.stringify(arrayUsuarios))
+    localStorage.setItem('usuarios', JSON.stringify(arrayUsuarios));
     MostrarData();
 }
 
+/*  Renderea los datos traidos desde localstorage, los guarda en el arrayUsuarios y los pinta mediante innerHTML en el elemento listaUsuarios*/
 MostrarData = () => {
 
     listaUsuariosUI.innerHTML = '';
@@ -40,63 +46,84 @@ MostrarData = () => {
     } else {
         arrayUsuarios.forEach(data => {
             listaUsuariosUI.innerHTML += `
-            <tr>
-                <td>${data.usuario} </td>
-                <td>${data.inicio}</td>
-                <td>${data.fin}</td>
-                <td>${data.tiempo}</td>
-                <td>$ ${data.tarifa}.00 MXN</td>
-                <td>
-                    <i class='material-icons mr-3'>
-                        schedule
-                    </i>
-                    <i class='material-icons ml-3'>
-                        delete
-                    </i>
-                </td>
-            </tr>`
+                <tr>
+                    <td>${data.usuario} </td>
+                    <td>${data.inicio}</td>
+                    <td>${data.fin}</td>
+                    <td>${data.tiempo}</td>
+                    <td>$ ${data.tarifa}.00 MXN</td>
+                    <td>
+                        <button class='btn btn-sm btn-success mr-2' data-id='${data.usuario}'>
+                            Finalizar
+                        </button>
+
+                        <button class='btn btn-sm btn-danger ml-2' data-id='${data.usuario}' id='borrar'>
+                            Borrar
+                        </button>
+                    </td>
+                </tr>`
         });
     }
 }
 
+/*
 
+    Guarda los datos de fechas, hace la diferencia entres las fechas con la librería momentjs, se realiza la lógica para obtener el costo.
+
+*/
 checarTiempo = (usuario) => {
-    //Encuentra el index del array
+    //Obtiene el index seleccionado del array
     let indexArray = arrayUsuarios.findIndex(
-        // (elemento) => elemento.usuario === usuario
         (elemento) => {
             return elemento.usuario === usuario
         }
     );
 
-
+    /*  Guarda fechas mediante momentjs */
     arrayUsuarios[indexArray].fin = moment().format('hh:mm:ss a');
-    arrayUsuarios[indexArray].finTiempo = moment().format('DD/MM/YYYY HH:mm:ss');
+    arrayUsuarios[indexArray].finTiempo = new Date();
+    /*  Guarda las fechas anteriores en el array de acuerdo al index seleccionado*/
     GuardarUsuario();
 
-    usuarios = JSON.parse(localStorage.getItem('usuarios'));
-    inicio = usuarios[indexArray].inicioTiempo;
-    fin = usuarios[indexArray].finTiempo;
+    /*  Obtiene datos desde localstorage */
+    inicio = arrayUsuarios[indexArray].inicioTiempo;
+    fin = arrayUsuarios[indexArray].finTiempo;
 
-    i = moment(inicio);
+    /* Convierte las fechas traidas desde localstoreage y las convierte a formato legible para momentjs */
     f = moment(fin);
+    i = moment(inicio);
 
-    diferencia =  moment.duration(f.diff(i));
-    final = diferencia.hours() + ' horas ' + ' : ' + diferencia.minutes() + ' minutos ';
+    /* Obtiene la diferencia entre las fechas anteriores */
+    diferencia = moment.duration(f.diff(i));
+
+    /* De la diferencia anterior se obtiene las horas y minutos */
+    final = diferencia.hours() + ':' + diferencia.minutes() + ' min. ';
+
+    /* Suma las horas y minutos para obtener minutos totales */
     minutosTotales = diferencia.hours() * 60 + diferencia.minutes();
+
+    /* Asigna valos al elemento tiempo */
     arrayUsuarios[indexArray].tiempo = final;
 
-    // Formula para obtener costo
-    costo = minutosTotales * 8 / 60;
-    // Asigna valor a elemento tarifa del objeto usuario
+    /* Obtiene el costo por el tiempo transcurrido */
+    costo = minutosTotales * 10 / 60;
+
+    /* Suma al costo dependiendo del tiempo trasncurrido */
+    if (minutosTotales <= 10) {
+        costo = costo + 2
+    } else if (minutosTotales >= 11 || minutosTotales <= 30) {
+        costo = costo + 1.5
+    }
+
+    /* Asigna valor a tarifa y el valor lo redondea al valor más próximo */
     arrayUsuarios[indexArray].tarifa = Math.round(costo);
 
-    // Guarda valores
+    /* Guarda valor de tarfifa anterior */
     GuardarUsuario();
 
 }
 
-// Elimina usuario de local storage
+/* Elimina usuario del localstorage */
 eliminarUsuario = (usuario) => {
     let indexArray;
     arrayUsuarios.forEach((item, index) => {
@@ -110,46 +137,53 @@ eliminarUsuario = (usuario) => {
     arrayUsuarios.splice(indexArray, 1);
     GuardarUsuario();
 }
+
 // EVENTLISTENER
 
+/* Crea y guarda el usuario mediante evento submit del formulario */
 formularioUI.addEventListener('submit', (e) => {
+
+    /* Mediante preventDefault() evita refrescar la página al ejecutar el evento submit */
     e.preventDefault();
     let usuarioUI = document.querySelector('#nombreUsuario').value;
 
+    /* Ejecuta métodos */
     CrearUsuario(usuarioUI);
     GuardarUsuario();
 
+    /* Limpia campo del formulario */
     formularioUI.reset();
 })
-// Mostrar datos
+
+/* Pinta los datos cuando se carga el DOM */
 document.addEventListener('DOMContentLoaded', MostrarData)
 
-//
-
+/* Ejecuta las acciones finalizar o eliminar */
 listaUsuariosUI.addEventListener('click', (e) => {
+
+    /* Mediante preventDefault() evita refrescar la página al ejecutar el evento click */
     e.preventDefault();
-    //
-    // console.log(e.target.innerHTML)
 
-    const accion = e.target.innerHTML.trim();
-    const accionFinal = e.path[2].childNodes[1].innerHTML.trim();
-    if (accion === 'schedule' || accion === 'delete') {
+    /* Obtiene el valor(texto) del elemento */
+    const accion = e.target.innerText;
 
+    /* Obtiene el id del por medio data-id  */
+    const id = e.target.dataset.id;
+
+    if (accion === 'Finalizar' || accion === 'Borrar') {
         switch (accion) {
-            case 'schedule':
-                checarTiempo(accionFinal);
+            case 'Finalizar':
+                checarTiempo(id);
                 break;
-
-            case 'delete':
-                eliminarUsuario(accionFinal);
+            case 'Borrar':
+                eliminarUsuario(id);
                 break;
         }
     }
 });
 
-
-// Limpiar cache localstorage
+/*  Limpia datos del localstorage */
 limpiarUI.addEventListener('click', (e) => {
     localStorage.clear();
     MostrarData();
-})
+});
